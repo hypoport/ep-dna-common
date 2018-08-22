c423e3ff7f0fd70ecb216f4e8821059cf1f263fc
jens.hanack <jens.hanack@hypoport.de>
2018-08-22 10:40:37 +0200

    package rename : dna.common

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='dna-common',
+    name='dna.common',
     url='https://github.com/hypoport/ep-dna-common',
     author='Jens Hanack',
     author_email='jens.hanack@europace.de',
     # Needed to actually package something
-    packages=['dna-common'],
+    packages=['dna.common'],
     # Needed for dependencies
     install_requires=['boto3'],
     version='0.1',
