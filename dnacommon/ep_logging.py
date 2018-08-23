8a496a4e40a4e79d047790fcc56606434e1424c4
jens.hanack <jens.hanack@hypoport.de>
2018-08-23 11:44:52 +0200

    Loglevel ist im logger nun konfigurierbar.

--- dnacommon/ep_logging.py
+++ dnacommon/ep_logging.py
@@ -2,6 +2,15 @@ import logging
 
 import sys
 
+CRITICAL = logging.CRITICAL
+FATAL = logging.FATAL
+ERROR = logging.ERROR
+WARN = logging.WARN
+WARNING = logging.WARNING
+INFO = logging.INFO
+DEBUG = logging.DEBUG
+NOTSET = logging.NOTSET
+
 _logging_nameToLevel = {
     'CRITICAL': logging.CRITICAL,
     'FATAL': logging.FATAL,
@@ -13,20 +22,26 @@ _logging_nameToLevel = {
     'NOTSET': logging.NOTSET,
 }
 
+
 def name_as_level(name: str):
     if name is not None:
         return _logging_nameToLevel.get(name.upper(), "INFO")
     else:
         return "INFO"
 
-def get_logger(name):
+
+def get_logger(name, loglevel=logging.DEBUG):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(loglevel)
     logger.propagate = False
     if not logger.handlers:
         ch = logging.StreamHandler(sys.stdout)
-        ch.setLevel(logging.DEBUG)
+        ch.setLevel(loglevel)
         formatter = logging.Formatter('[%(levelname)s] %(asctime)s - %(name)s - %(message)s')
         ch.setFormatter(formatter)
         logger.addHandler(ch)
     return logger
+
+
+if __name__ == '__main__':
+    pass
