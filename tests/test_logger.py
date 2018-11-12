c647c89c2fe2e38051df951bbb2c97102b28c083
bweigel <benjamin.weigel@europace.de>
2018-11-12 15:48:02 +0100

    implements logger with timestamp and milliseconds

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -3,12 +3,21 @@ import logging
 from dnacommon.ep_logging import name_as_level
 
 
-@pytest.mark.parametrize("test_input, expected", [
-    ("deBug", logging.DEBUG),
-    ("debug", logging.DEBUG),
-    ("ERROR", logging.ERROR),
-    ("Deb", logging.INFO),
-    (None, logging.INFO)
-])
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("deBug", logging.DEBUG),
+        ("debug", logging.DEBUG),
+        ("ERROR", logging.ERROR),
+        ("Deb", logging.INFO),
+        (None, logging.INFO),
+    ],
+)
 def test_name_as_level(test_input, expected):
     assert name_as_level(test_input) is expected
+
+
+def test_log_has_tz_info():
+    LOG = get_logger("abc")
+    LOG.error("Test")
+    assert False
