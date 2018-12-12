831cb4d59a9ef8923340027ccd9242f7442a4167
jens.hanack <jens.hanack@hypoport.de>
2018-12-12 10:00:02 +0100

    Test für Presigned s3 urls ergänzt

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -7,8 +7,15 @@ from dnacommon.ep_logging import name_as_level
 
 
 def test_upload_s3():
-    bucket = 'europace.temporary'
+    bucket = 'europace.dna.labelingtool.dokdownload'
     path = 'test/s3clienttest.txt'
     content = 's3clienttest text'
     S3Client().upload(bucket, path, content, acl='bucket-owner-full-control')
     S3Client().upload(bucket, path, content)
+
+
+def test_presigned_url():
+    bucket = 'europace.dna.labelingtool.dokdownload'
+    path = 'test/reisepass_mustermann.pdf'
+    client = S3Client()
+    print(client.generate_presigned_get_url(bucket, path, 3000))
