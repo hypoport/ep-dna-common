3c0fcca1b4ab285af1fc53f4df194e8056e9d748
nikolaus.werner <nikolaus.werner@hypoport.de>
2019-09-03 14:00:15 +0200

    aws resources for test mocked

--- dnacommon/aws/redshift/test_connection.py
+++ dnacommon/aws/redshift/test_connection.py
@@ -1,14 +1,30 @@
 import json
 
 import pkg_resources
+import pytest
 
 from dnacommon.aws.redshift.redshift import RedshiftConfiguration
 
 
-# @pytest.mark.skip("Needs to be run with aws profile on dev account")
-def test_config_secret_name():
+@pytest.fixture
+def mock_redshift_connector(mocker: object) -> object:
+    mocker.patch.object(RedshiftConfiguration, 'get_secret')
+
+    RedshiftConfiguration.get_secret.return_value = {'password': 'PW', 'user': 'USER'}
+
+    yield
+    RedshiftConfiguration.get_secret.assert_called_with('ep-dna-reporting-redshift-connection-dev')
+
+
+def test_config_secret_name(mock_redshift_connector):
     conf_dict = json.loads(pkg_resources.resource_string(__name__, "resources/conf.test_secret_name.json"))
     redshift = RedshiftConfiguration(**conf_dict)
     assert redshift.secret_name == "ep-dna-reporting-redshift-connection-dev"
     assert redshift.password is not None
     assert redshift.encrypted_password is None
+    assert redshift.port == 10000
+    assert redshift.iam_role == 'ROLE'
+    assert redshift.host == 'HOST'
+    assert redshift.database == 'reporting_test'
+    assert redshift.username == 'test_user'
+    assert redshift.password == 'PW'
